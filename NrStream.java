756531ab56f89d1bbbfd609a31401ff6b6ffad26
Faizi5807 <l215807@lhr.nu.edu.pk>
2023-10-12 22:56:13 -0700

    Levi Ackerman

--- NrStream.java
+++ NrStream.java
@@ -1,14 +1,51 @@
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+
+import java.util.LinkedHashSet;
+
 import java.util.Map;
 
 public class NrStream {
-    private Map<Character, Integer> mapStream;
-    private List<Character> nrStream;
+    public Map<Character, Integer> mapStream;
+    public LinkedHashSet<Character> nrStream;
 
     public NrStream() {
         mapStream = new HashMap<>();
-        nrStream = new ArrayList<>();
+        nrStream = new LinkedHashSet<>();
+    }
+
+    public void add(char character) {
+        mapStream.put(character, mapStream.getOrDefault(character, 0) + 1);
+        // int valueToPut=0;
+
+        boolean check = (mapStream.get(character) > 1);
+        if (!check) {
+            nrStream.add(character);
+        } else {
+            nrStream.remove(character);
+            return;
+        }
+    }
+
+    public char getFirstNonRepeating() {
+        char firstNonRepeating = '-';
+        for (char firstOnly : nrStream) {
+            firstNonRepeating = firstOnly;
+            break;
+        }
+        return firstNonRepeating;
+    }
+
+    public static void main(String[] args) {
+        NrStream stream = new NrStream();
+        stream.add('a');
+        stream.add('b');
+        stream.add('c');
+        stream.add('d');
+        stream.add('a');
+        stream.add('b');
+        // stream.add('c');
+        System.out.println(stream.nrStream);
+        System.out.println(stream.mapStream);
+        System.out.println(stream.getFirstNonRepeating());
     }
 }
